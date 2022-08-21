79b4ef855eb2bfde6f71fad9200f9711469196ea
AbdullahHafez <abdullah.alaa12@gmail.com>
2022-08-21 19:43:22 +0200

    feat: Add courses data array to app.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const coursesData = [
+    {
+      id: 1,
+      image: "https://img-c.udemycdn.com/course/750x422/394676_ce3d_5.jpg",
+      title: "Learn Python: The Complete Python Programmming Course",
+      author: "Avinash Jain, The Codex",
+      rating: 4.4,
+      no_voters: 2845,
+      price: 199.99,
+      old_price: 679.99,
+      bestSeller: true,
+    },
+    {
+      id: 2,
+      image: "https://img-c.udemycdn.com/course/750x422/396876_cc92_7.jpg",
+      title: "Learning Python for Data Analysis and Visualization",
+      author: "Jose Portilla",
+      rating: 4.4,
+      no_voters: 17956,
+      price: 269.99,
+      old_price: 1599.99,
+      bestSeller: false,
+    },
+    {
+      id: 3,
+      image: "https://img-c.udemycdn.com/course/750x422/405878_e5a0_3.jpg",
+      title: "Python for Beginners - Learn Programming from scratch",
+      author: "Edwin Diaz, Coding Faculty Solutions",
+      rating: 4.4,
+      no_voters: 1734,
+      price: 199.99,
+      old_price: 679.99,
+      bestSeller: false,
+    },
+    {
+      id: 4,
+      image: "https://img-c.udemycdn.com/course/750x422/426570_1b91_3.jpg",
+      title: "Learn Python: Python for Beginners",
+      author: "Abrar Hussain",
+      rating: 4.2,
+      no_voters: 2759,
+      price: 199.99,
+      old_price: 319.99,
+      bestSeller: false,
+    },
+    {
+      id: 5,
+      image: "https://img-c.udemycdn.com/course/750x422/449532_2aa9_7.jpg",
+      title: "Python Beyond the Basics - Object-Oriented Programming",
+      author: "Infinite Skills",
+      rating: 4.4,
+      no_voters: 2927,
+      price: 229.99,
+      old_price: 519.99,
+      bestSeller: false,
+    },
+  ];
+
   return <></>;
 }
 
