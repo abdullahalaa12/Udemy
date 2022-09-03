5adddef2247ddb50620ea6ff8316d851e5711cc6
AbdullahHafez <abdullah.alaa12@gmail.com>
2022-09-03 21:48:47 +0200

    feat: Create and style page not found page

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "../style_modules/PageNotFound.module.css";
 
 function PageNotFound() {
-  return <h1>PageNotFound</h1>;
+  return (
+    <main className={styles["main"]}>
+      <img
+        alt="Page not found"
+        src="https://s.udemycdn.com/error_page/error-desktop-v1.jpg"
+      />
+      <h1 className={styles["heading"]}>
+        We can’t find the page you’re looking for
+      </h1>
+      <p className={styles["paragraph"]}>
+        Visit our <a href="#">support page</a> for further assistance.
+      </p>
+    </main>
+  );
 }
 
 export default PageNotFound;
