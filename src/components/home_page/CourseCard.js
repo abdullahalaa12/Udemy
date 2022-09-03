97af6a8d90a5d534b5e07d92993d3332364d0a31
AbdullahHafez <abdullah.alaa12@gmail.com>
2022-09-03 21:47:48 +0200

    refactor: Create styles modules files

--- src/components/home_page/CourseCard.js
+++ src/components/home_page/CourseCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../../style_modules/CourseCardStyles.module.css";
+import styles from "../../style_modules/home_page/CourseCardStyles.module.css";
 import RatingContainer from "./RatingContainer";
 
 function CourseCard(props) {
