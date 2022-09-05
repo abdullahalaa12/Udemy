5a5fcd116aa7accc0247db2bd5a0f44525baadab
AbdullahHafez <abdullah.alaa12@gmail.com>
2022-09-05 17:31:30 +0200

    feat: Create Side bar and adjust the position

---
+++ src/components/course_page/SideBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../../style_modules/course_page/SideBar.module.css";
+
+function SlideBar() {
+  return <div className={styles["sidebar"]}></div>;
+}
+
+export default SlideBar;
