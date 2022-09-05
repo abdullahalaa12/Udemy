0311ee776f65b361867e772c659665c9d6459812
AbdullahHafez <abdullah.alaa12@gmail.com>
2022-09-05 14:59:12 +0200

    feat: Create Review section

--- src/components/course_page/Reviews.js
+++ src/components/course_page/Reviews.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Review from "./Review";
+import styles from "../../style_modules/course_page/Reviews.module.css";
 
 function Reviews(props) {
   const { reviews } = props;
 
-  return <div>{reviews.count}</div>;
+  return (
+    <div className={styles["container"]}>
+      {reviews.results.map((review, index) => (
+        <Review key={index} review={review} />
+      ))}
+    </div>
+  );
 }
 
 export default Reviews;
